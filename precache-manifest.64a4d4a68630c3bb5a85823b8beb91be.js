cc7e0dfa3d7435947636d711cb156d39b62f9c69
richard <richardmilton@ahotclub.com>
2019-05-04 06:18:20 +0100

    Updates

--- precache-manifest.64a4d4a68630c3bb5a85823b8beb91be.js
+++ precache-manifest.64a4d4a68630c3bb5a85823b8beb91be.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/stamp-duty-web/static/js/runtime~main.53293ddb.js"
   },
   {
-    "revision": "62a2ade8931213ba5483",
-    "url": "/stamp-duty-web/static/js/main.b47fcae2.chunk.js"
+    "revision": "a2075db78cfbb1526d7a",
+    "url": "/stamp-duty-web/static/js/main.39fdd423.chunk.js"
   },
   {
     "revision": "63ae5fc7047a647d90cf",
     "url": "/stamp-duty-web/static/js/2.a1ef7eec.chunk.js"
   },
   {
-    "revision": "62a2ade8931213ba5483",
+    "revision": "a2075db78cfbb1526d7a",
     "url": "/stamp-duty-web/static/css/main.a4208b0f.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/stamp-duty-web/static/css/2.87813b20.chunk.css"
   },
   {
-    "revision": "5a109f904e783847df53ff4a78b71ce7",
+    "revision": "b7646ac2698854c9bb7d335664317e53",
     "url": "/stamp-duty-web/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/stamp-duty-web/precache-manifest.30a0ac9fa68e47e061ec9ffcf8eadc0f.js"
+  "/stamp-duty-web/precache-manifest.64a4d4a68630c3bb5a85823b8beb91be.js"
 );
 
 workbox.clientsClaim();
